63c9b923e2d2803c0c2449a416e7bc60a6c08d0b
rsrawatofficial <rsrasasingh@gmail.com>
2025-04-30 14:28:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ API_ID = os.environ.get("API_ID", "27900743")
 
 API_HASH = os.environ.get("API_HASH", "ebb06ea8d41420e60b29140dcee902fc")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8003649544:AAGoiThVN8KLJyKGsGf1BcfTsjDTrSmjFR8")
 
 token_cp = os.environ.get("token_cp", "")
 
